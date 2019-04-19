7ef16134ea4e65b16705e01e2756e1245bdcf603
destin <destintech@gmail.com>
2019-04-19 11:56:57 -0600

    Added http's' to stop mixed resource loading error

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component{
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
     if (city && country){
     console.log(data);
@@ -39,7 +39,7 @@ class App extends React.Component{
     });
   }
 
-  
+
   else{
     this.setState({
       temperature:undefined,
